e90a4a44c0e6132d4d16664d6622d1018c3fa648
HeadCliffer <57980621+HeadCliffer@users.noreply.github.com>
2019-11-21 12:42:40 +0300

    Add files via upload

--- Level1.java
+++ Level1.java
@@ -1,25 +1,17 @@
-
 import java.math.BigInteger;
-import java.util.*;
-public static class main ( String[] args){
 
-        int crackedNuts = squirrel(2);
-
-
-        }
 public class Level1 {
-    private static BigInteger factorial = BigInteger.valueOf(1);
+    public static BigInteger factorial = BigInteger.ONE;
 
     public static int squirrel(int N) {
-        if(N==0) return 1; // 0! = 1;
+        if(N<0) return Integer.parseInt(null);
+        if( N==0 || N==1 ) return 1; // 0! = 1;
         else{
-            for( int i = 1; i<=N; i++ ) {
+            for( int i = 2; i<=N; i++ ) {
                 factorial = factorial.multiply(BigInteger.valueOf(i));
                 System.out.println(factorial);
             }
-            char[] emeralds = factorial.toString().toCharArray();
-            System.out.println(emeralds[0]);
-            return Integer.parseInt(String.valueOf(emeralds[0]));
+            return Integer.parseInt( String.valueOf( factorial.toString().charAt(0) ));
         }
     }
 }
