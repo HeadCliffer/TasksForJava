92dd782201d207d79b2b3440875ae9a629bb5be0
HeadCliffer <57980621+HeadCliffer@users.noreply.github.com>
2019-11-21 13:03:39 +0300

    Add files via upload

--- Level1.java
+++ Level1.java
@@ -5,11 +5,10 @@ public class Level1 {
 
     public static int squirrel(int N) {
         if(N<0) return Integer.parseInt(null);
-        if( N==0 || N==1 ) return 1; // 0! = 1;
+        else if( N==0 || N==1 ) return 1; // 0! = 1;
         else{
             for( int i = 2; i<=N; i++ ) {
                 factorial = factorial.multiply(BigInteger.valueOf(i));
-                System.out.println(factorial);
             }
             return Integer.parseInt( String.valueOf( factorial.toString().charAt(0) ));
         }
