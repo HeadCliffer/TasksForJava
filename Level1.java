f9ae783ded91aa7cd20261d07a053c8e731f09f5
HeadCliffer <57980621+HeadCliffer@users.noreply.github.com>
2019-11-21 14:06:34 +0300

    Add files via upload

---
+++ Level1.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+
+public class Level1 {
+    private static BigInteger factorial = BigInteger.ONE;
+
+    public static int squirrel(int N) {
+        if(N<0) return Integer.parseInt(null);
+        else if( N==0 || N==1 ) return 1; // 0! = 1;
+        else if( N==2 ) return 2;
+        else{
+            for( int i = 2; i<=N; i++ ) {
+                factorial = factorial.multiply(BigInteger.valueOf(i));
+            }
+            return Integer.parseInt( String.valueOf( factorial.toString().charAt(0) ));
+        }
+    }
+}
+
+
