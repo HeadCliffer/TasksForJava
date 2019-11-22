30f9391e754de57564cb1402c03fbbfa058ebaa7
HeadCliffer <57980621+HeadCliffer@users.noreply.github.com>
2019-11-22 12:55:24 +0300

    Add files via upload

--- Level1.java
+++ Level1.java
@@ -1,9 +1,9 @@
 public class Level1 {
-    private static int odometer(int[] oksana){
+    public static int odometer(int[] oksana) {
         int result = 0;
-        for( int i = 0; i<oksana.length; i++ ){
-            if( oksana[i] < 0 ) return result;
-            if(i % 2 == 0) result += oksana[i];
+        for (int i = 0; i < oksana.length; i++) {
+            if (oksana[i] < 0) return result;
+            if (i % 2 == 0) result += oksana[i];
         }
         return result;
     }
