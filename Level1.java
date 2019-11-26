3b8d013e54233963ed50c1eb3c58af7a3ce2d0ea
HeadCliffer <57980621+HeadCliffer@users.noreply.github.com>
2019-11-26 05:57:54 +0300

    Add files via upload

---
+++ Level1.java
@@ -0,0 +1,81 @@
+public class Level1 {
+    
+    /*public static void main(String[] args){
+
+        String s = "строка разбивается на набор строк через выравнивание по заданной ширине.";
+        String subs = "строк";
+        WordSearch(12, s, subs );
+
+    }*/
+
+    public static int [] WordSearch(int len, String s, String subs){
+        int spaceLength = 0;
+        int arrayCount = 1;
+        String arrayNumbers = "";
+        String sCopy = "";
+        String ln = "\n";
+        char[] ch = s.trim().toLowerCase().toCharArray();
+
+        // add \n
+        for(int i = 0; i<ch.length; i++) {
+            spaceLength++;
+
+            if(ch[i] == ' '  && spaceLength > len/2 ) {
+                i++;
+                sCopy += ln;
+                spaceLength = 0;
+                arrayCount++;
+            }
+
+            if(spaceLength == len) {
+                i--;
+                sCopy += ln;
+                spaceLength = 0;
+                arrayCount++;
+            }
+
+            else sCopy += ch[i];
+        }
+
+        /*System.out.println(sCopy);
+        System.out.println(arrayCount);
+        System.out.println(arrayNumbers);
+        System.out.println("**************");*/
+
+        int[] numbers = new int[arrayCount];
+        char[] chCopy = sCopy.toCharArray();
+        String word = "";
+        int count = 0;
+
+        //Check subs word
+        for (int i = 0; i < chCopy.length; i++) {
+
+            if(chCopy[i] == '\n'){
+                i++;
+                //System.out.println(word);
+                if(word.contentEquals(subs)){
+                    numbers[count] = 1;
+                    count++;
+                    word = "";
+                }else{
+                    count++;
+                    word = "";
+                }
+            }
+            if(chCopy[i] == ' ' || i == chCopy.length - 1 ){
+                //System.out.println(word);
+                if(word.contentEquals(subs)) {
+                    numbers[count] = 1;
+                    word = "";
+                }else word = "";
+            }
+            else word += chCopy[i];
+        }
+
+        /*for (int i = 0; i <numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }*/
+
+        return numbers;
+    }
+}
